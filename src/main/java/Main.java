cc6619b2ae600d7c26794d8620880d1ca43c11b2
Rafael Felipe <rafaelrfq@gmail.com>
2020-07-25 18:54:34 -0400

    Initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,20 @@
+import org.jpl7.Query;
+import org.jpl7.Term;
+
+import java.util.Iterator;
+import java.util.Map;
+
+public class Main {
+    public static void main(String[] args) {
+        Term t = Term.textToTerm("consult('db.pl')");
+        Query.hasSolution(t);
+
+        Iterator<Map<String, Term>> solutionsIter = new Query("person(X, Y, Z)");
+        while (solutionsIter.hasNext()) {
+            Map<String, Term> sol = solutionsIter.next();
+            System.out.println("\t Solution found (now asserting to block?): " + sol.toString());
+            Query.oneSolution(String.format("assertz(name_of_person(%s))", sol.get("X").toString()));
+        }
+        Query.hasSolution("listing(name_of_person/1)");
+    }
+}
